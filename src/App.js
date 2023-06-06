be953c2452347d9e7b2aaa4aff1ade4bdc7a3759
islam-24-03 <isabhnisa@gmail.com>
2023-06-06 16:09:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class component extends React.Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.data !== prevProps.data) {
+      this.fetchData();
+    }
+  }
+
+  fetchData() {
+
+  }
+
+  render() {
+    return <div>{/* Рендеринг компонента */}</div>;
+  }
 }
 
-export default App;
+export default component;
+
+
+
+
+
+//1)class MyComponent extends React.Component {
+//componentDidMount() {
+//this.inputRef.focus();
+//}
+
+//render() {
+//return (
+//<div>
+//<input ref={(ref) => (this.inputRef = ref)} />
+//</div>
+//);
+//}
+//}
+
+
+//export default MyComponent;
